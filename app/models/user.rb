239caa651cb83a3a67bf3a20cfea21787dc22431
igcs15 <80236172+igcs15@users.noreply.github.com>
2021-03-14 16:18:57 +0530

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
 validates :firstname, :lastname, :email, presence: true
+validates :email, format: { with: URI::MailTo::EMAIL_REGEXP },confirmation: { case_sensitive: false },uniqueness: true
 end
